915551cda7d366bbb959ae0edd741a8656fdc6bc
lejitao2013 <61481263+lejitao2013@users.noreply.github.com>
2022-05-10 00:20:36 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -9,3 +9,6 @@
 sed -i 's/192.168.1.1/192.168.50.1/g' openwrt/package/base-files/files/bin/config_generate
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+#3 Set DISTRIB_REVISION
+sed -i "s/OpenWrt /TAO Build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+
